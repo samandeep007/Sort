be628f964450db2a196332ce19b7ca135c67febe
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-22 16:49:07 -0600

    Refactor InsertionSort class to improve sorting algorithm

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class InsertionSort {
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void insertionSort(int[] arr){
+        for(int i = 0; i<arr.length - 1; i++){
+            for(int j = i + 1; j>0; j--){
+                if(arr[j-1] > arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
 }
