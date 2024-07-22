b112e1a555c402a81026f9d128f66ad515e3091d
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-22 14:59:01 -0600

    Refactor SelectionSort and BubbleSort classes to use optimized sorting algorithms

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args){
         int[] arr = {4,5,8,3,2,54,32,67,31,16};
-        selectionSort(arr);
+        selectionSort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -47,19 +47,54 @@ public class SelectionSort {
     //     }
     // }
 
-  static void selectionSort(int[] arr){
-        for(int i = 0; i<arr.length - 1; i++){
+    static void selectionSort(int[] arr) {
+        // Outer loop to iterate over each element of the array except the last one
+        for (int i = 0; i < arr.length - 1; i++) {
+            // Assume the maximum element is at the start of the unsorted portion
             int maxIndex = 0;
-            for(int j = 0; j < arr.length - i ; j++){
-                if(arr[j] > arr[maxIndex]){
+
+            // Inner loop to find the maximum element in the unsorted portion
+            for (int j = 1; j < arr.length - i; j++) {
+                // Update maxIndex if a larger element is found
+                if (arr[j] > arr[maxIndex]) {
                     maxIndex = j;
                 }
             }
-                int temp = arr[maxIndex];
-                arr[maxIndex] = arr[arr.length - i - 1];
-                arr[arr.length - i - 1] = temp;
+
+            // Swap the found maximum element with the element at the end of the unsorted portion
+            // Position to swap with is `arr.length - i - 1`
+
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
         }
-  }
+    }
+
+
+    // using the minimum approach
+
+    static void selectionSort2(int[] arr) {
+        // Outer loop to iterate through each element of the array
+        for (int i = 0; i < arr.length; i++) {
+            // Assume the minimum element is at the current position i
+            int minIndex = i;
+
+            // Inner loop to find the minimum element in the remaining unsorted portion
+            for (int j = i + 1; j < arr.length; j++) {
+                // Update minIndex if a smaller element is found
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the element at the current position i
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
 
 
 
