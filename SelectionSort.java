8c4a10d1c64db478e5743cb3dd24976f664f4a5a
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-22 15:36:25 -0600

    Refactor SelectionSort class to use optimized sorting algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args){
         int[] arr = {4,5,8,3,2,54,32,67,31,16};
-        selectionSort(arr);
+        selection(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -67,6 +67,30 @@ public class SelectionSort {
                 arr[minIndex] = temp;
             }
         }
+    }
+
+    // Kunal's way of doing this
+    static void selection(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0,last);
+            swap(arr, maxIndex, last);
+
+        }
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    static int getMaxIndex(int[] arr, int start, int end){
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if(arr[i] > arr[maxIndex] ){
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 
 
